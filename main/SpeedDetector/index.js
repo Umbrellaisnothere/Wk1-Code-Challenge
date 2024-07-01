6632bb1cdd5ffc3da8c2458cc724774c9b3c6fce
Keith Murimi <keith.murimi@student.moringaschool.com>
2024-07-01 13:07:22 +0300

    Updated the condition

--- main/SpeedDetector/index.js
+++ main/SpeedDetector/index.js
@@ -4,8 +4,6 @@ function speedDetector(speed) {
 
     //Expressing the  demeritpoint
     const kmPerDemeritPoint = 5;
-
-    let points = 2;
     
     //Condition
     if (speed < speedLimit) {
@@ -13,11 +11,14 @@ function speedDetector(speed) {
     } 
     else 
     {
-        points = Math.floor((speed - speedLimit) / kmPerDemeritPoint);
-        console.log(`Points: ${points}`);
+       const points = Math.floor((speed - speedLimit) / kmPerDemeritPoint);
+        console.log(`Points: ${points}.`);
         
         if (points >= 12) {
             console.log("License suspended");
         }
     }
 }
+
+// Input a number to determine the status of the vehicle
+(speedDetector());
